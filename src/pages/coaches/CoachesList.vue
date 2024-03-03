8e6848fecf6301e31035a5b80d0112feeacf9788
alexanderkuzmenko <57828759+Haarkin@users.noreply.github.com>
2024-03-03 17:37:44 +0200

    Project finished

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,14 +1,21 @@
 <template>
+    <div>
+    <base-dialog :show="!!error" title="An error occured." @close="handleError">
+        <p>{{ error }}</p>
+    </base-dialog>
     <section>
         <coach-filter @change-filter="setFilter"></coach-filter>
     </section>
     <section>
         <base-card>
             <div class="controls">
-                <base-button mode="outline">Refresh</base-button>
-                <base-button v-if="!isCoach" link to="/register">Register as Coach</base-button>
+                <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
+                <base-button v-if="!isCoach && !isLoading" link to="/register">Register as Coach</base-button>
             </div>
-            <ul v-if="hasCoaches">
+            <div v-if="isLoading">
+                <base-spinner></base-spinner>
+            </div>
+            <ul v-else-if="hasCoaches">
             <coach-item v-for="coach in filteredCoaches" 
                 :key="coach.id" 
                 :id="coach.id" 
@@ -23,6 +30,7 @@
             <h3 v-else>No coaches found</h3>
         </base-card>
     </section>
+</div>
 </template>
 
 <script>
@@ -36,6 +44,8 @@ export default {
     },
     data() {
         return {
+            isLoading: false,
+            error: null,
             activeFilters: {
                 frontend: true,
                 backend: true,
@@ -64,11 +74,26 @@ export default {
             })
         },
         hasCoaches() {
-            return this.$store.getters['coaches/hasCoaches'];
+            return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
+    },
+    created() {
+        this.loadCoaches()
     },
     methods: {
         setFilter(updatedFilters) {
             this.activeFilters = updatedFilters;
+        },
+        async loadCoaches(refresh = false) {
+            this.isLoading = true;
+            try {
+                await this.$store.dispatch('coaches/loadCoaches', {forceRefresh: refresh});
+            } catch (error) {
+                this.error = error.message || 'Something vent wrong.'
+            }
+            this.isLoading = false;
+        },
+        handleError() {
+            this.error = null;
         }
     }
 }
